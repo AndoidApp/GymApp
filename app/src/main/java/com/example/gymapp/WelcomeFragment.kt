94be3d7a0c7edcd6dcde34a3f1ddf69257e9a733
Matteo Naccarato <matteo.naccarato@itiscuneo.eu>
2024-05-18 23:20:06 +0200

    Fix navigation between fragments

--- app/src/main/java/com/example/gymapp/WelcomeFragment.kt
+++ app/src/main/java/com/example/gymapp/WelcomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
 class WelcomeFragment : Fragment() {
 
     override fun onCreateView(
@@ -17,6 +20,9 @@ class WelcomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+
+        view.findViewById<Button>(R.id.btn_login).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.welcome2login)
+        }
     }
 }
